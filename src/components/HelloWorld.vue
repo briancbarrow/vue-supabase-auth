0c314c9d3ca05c52ca497e4cca7f5b488e1fb6ef
Brian Barrow <briancbarrow@gmail.com>
2022-02-18 14:04:18 -0700

    updated initial styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,34 +13,9 @@ async function signOut() {
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="hello-world">{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-
-  <button @click="signOut">Sign Out</button>
+  <button class="signout-button" @click="signOut">Sign Out</button>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
